eceec11334f98217647dfaa13c885fdec3780b7b
teekay24 <tkrafty24@gmail.com>
2024-12-12 19:46:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
 function displayPhotos(photos) {
     const photoGallery = document.getElementById('photo-gallery');
     photoGallery.innerHTML = ''; // Clear the gallery
+const photoPath = `./photos/${photo.Photo_ID}.jpg`;
+console.log(`Constructed photo path: ${photoPath}`);
 
     // Display each photo
     photos.forEach(photo => {
